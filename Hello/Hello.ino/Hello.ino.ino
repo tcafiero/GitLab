6eea852443fd9a122edc9d9d458c5598d3f6a1e9
Toni Cafiero <toni@cafiero.it>
2016-03-19 21:22:10 +0100

    comments added

--- Hello/Hello.ino/Hello.ino.ino
+++ Hello/Hello.ino/Hello.ino.ino
@@ -7,6 +7,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   Serial5.println("Hello World!");
   delay(1000);
+  /* two print Miltitask! */
   Serial5.println("Multitask!");
   Serial5.println("Multitask!");
 }
